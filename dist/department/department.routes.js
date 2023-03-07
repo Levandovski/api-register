0285416158f0826c19973a79f060099582991f33
JesJessé Levandovski <jesse.levandovski@outlook.com>
2023-03-06 22:41:51 -0300

    remove dist

--- dist/department/department.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const department_controllers_1 = require("./department.controllers");
-const departmentRouter = (0, express_1.Router)();
-departmentRouter.post("/", department_controllers_1.createDepartmentController);
-exports.default = departmentRouter;
--- dist/employee/employee.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const employee_controllers_1 = require("./employee.controllers");
-const employeeRouter = (0, express_1.Router)();
-employeeRouter.post("/", employee_controllers_1.createEmployeeController);
-exports.default = employeeRouter;
